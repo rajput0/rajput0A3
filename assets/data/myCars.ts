16a9d8e42895aef7087de70dffdc582e50537f0d
rajput0 <50675916+rajput0@users.noreply.github.com>
2020-07-29 11:30:27 -0400

    Add files via upload

---
+++ assets/data/myCars.ts
@@ -0,0 +1,9 @@
+
+import { carInfo } from '../../app/carInfo';
+
+export const MYCLASSES : carInfo[] = [
+    { make: 'Mustang', model: 1970,  bodyType: 'Sedan', color: 'Red', picture: '../assets/images/mustang.jpg' },
+    { make: 'Rolls Royce', model: 2018,  bodyType: 'Sedan', color: 'Grey', picture: '../assets/images/rollsroyce.jpg' },
+    { make: 'Mercedes', model: 2018,  bodyType: 'Sedan', color: 'Brown', picture: '../assets/images/mercedes.jpg' },
+    { make: 'Lamborghini', model: 2019,  bodyType: 'Sedan', color: 'White', picture: '../assets/images/lamborgini.jpg' }
+]
